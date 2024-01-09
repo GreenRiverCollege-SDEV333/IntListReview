2dd7c7161ce9eb3f2cd5ec1ebd8cfbb9fa77dd7c
William Castillo <william.castillo.jr@outlook.com>
2024-01-09 14:34:21 -0800

    Created a private helper Iterator class, contains implements hasNext and next methods.

--- src/ArrayIntList.java
+++ src/ArrayIntList.java
@@ -1,4 +1,5 @@
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class ArrayIntList implements IntList
 {
@@ -245,4 +246,49 @@ public class ArrayIntList implements IntList
     {
         return null;
     }
+
+    //create a private helper Iterator class
+    private class IntListIterator implements Iterator<Integer> {
+        //private fields:
+        private int i;
+
+        public IntListIterator()
+        {
+            i = 0;
+        }
+        /**
+         * Returns {@code true} if the iteration has more elements.
+         * (In other words, returns {@code true} if {@link #next} would
+         * return an element rather than throwing an exception.)
+         *
+         * @return {@code true} if the iteration has more elements
+         */
+        @Override
+        public boolean hasNext()
+        {
+            return i < size;
+        }
+
+        /**
+         * Returns the next element in the iteration.
+         *
+         * @return the next element in the iteration
+         * @throws NoSuchElementException if the iteration has no more elements
+         */
+        @Override
+        public Integer next()
+        {
+            if (i >= size)
+            {
+                throw new NoSuchElementException("i is now out of bounds");
+            }
+            int currentValue = buffer[i];
+            i++;
+
+            return currentValue;
+        }
+    }
+
+    // iterators are what enables main/client to use a for-each loop
+    // on my IntList
 }
