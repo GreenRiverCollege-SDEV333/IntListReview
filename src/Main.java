dae3a92f52719728c52f6e30ebf1b7ad83c1b4ff
Huy Nguyen <hnguyen10082000@gmail.com>
2024-01-09 13:18:26 -0800

    Added starter code, finished some easy methods

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,19 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
 
+        ArrayIntList my = new ArrayIntList();
+        my.addFront(10);
+        my.addFront(-2);
+
+        System.out.println(my.indexOf(1));
+
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
+
+
+
         }
     }
 }
