a32694936d9c28d4ea9a20aafac24054137bb1b6
RJ Trenchard <rjtrenchard@gmail.com>
2024-01-22 14:44:22 -0800

    add test method for linked list, implement add

---
+++ test/LinkedIntListTest.java
@@ -0,0 +1,56 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class LinkedIntListTest {
+
+    @org.junit.jupiter.api.Test
+    void addFront() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addBack() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void add() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void removeFront() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void removeBack() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void remove() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void get() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void contains() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void indexOf() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void isEmpty() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void size() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void clear() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void iterator() {
+    }
+}
