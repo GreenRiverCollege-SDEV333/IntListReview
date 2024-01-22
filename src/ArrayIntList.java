aeafcd0897f03cb60183eda7b274a689370e8815
BuildTools <unconfigured@null.spigotmc.org>
2024-01-22 15:52:03 -0800

    Filled out ArrayIntList class

---
+++ src/ArrayIntList.java
@@ -0,0 +1,247 @@
+import java.util.Iterator;
+
+public class ArrayIntList implements IntList
+{
+
+    private int size;
+    private int[] buffer;
+
+    public ArrayIntList(){
+        this.size = 0;
+        this.buffer = new int[10];
+    }
+
+    /**
+     * Prepends (inserts) the specified value at the front of the list (at index 0).
+     * Shifts the value currently at the front of the list (if any) and any
+     * subsequent values to the right.
+     *
+     * @param value value to be inserted
+     */
+    @Override
+    public void addFront(int value)
+    {
+        if(size == buffer.length){
+            size = size * 2;
+        }
+
+
+
+        for (int i = size; i >= 1; i--)
+        {
+            buffer[i] = buffer[i-1];
+        }
+        buffer[0] = value;
+        size++;
+    }
+
+    /**
+     * Appends (inserts) the specified value at the back of the list (at index size()-1).
+     *
+     * @param value value to be inserted
+     */
+    @Override
+    public void addBack(int value)
+    {
+        if(size == buffer.length){
+            size = size * 2;
+        }
+
+        buffer[size] = value;
+        size++;
+    }
+
+    /**
+     * Inserts the specified value at the specified position in this list.
+     * Shifts the value currently at that position (if any) and any subsequent
+     * values to the right.
+     *
+     * @param index index at which the specified value is to be inserted
+     * @param value value to be inserted
+     * @throws IndexOutOfBoundsException if the index is out of range
+     */
+    @Override
+    public void add(int index, int value)
+    {
+        for (int i = size; i > index; i--) {
+            buffer[i] = buffer[i - 1];
+        }
+        buffer[index] = value;
+        size++;
+    }
+
+    /**
+     * Removes the value located at the front of the list
+     * (at index 0), if it is present.
+     * Shifts any subsequent values to the left.
+     */
+    @Override
+    public void removeFront()
+    {
+        for (int i = 0; i < size - 1; i++)
+        {
+            buffer[i] = buffer[i+1];
+        }
+        size--;
+    }
+
+    /**
+     * Removes the value located at the back of the list
+     * (at index size()-1), if it is present.
+     */
+    @Override
+    public void removeBack()
+    {
+        buffer[size] = 0;
+        size--;
+    }
+
+    /**
+     * Removes the value at the specified position in this list.
+     * Shifts any subsequent values to the left. Returns the value
+     * that was removed from the list.
+     *
+     * @param index the index of the value to be removed
+     * @return the value previously at the specified position
+     * @throws IndexOutOfBoundsException if the index is out of range
+     */
+    @Override
+    public int remove(int index)
+    {
+        int removed = buffer[index];
+
+        for (int i = index; i < size - 1; i++) {
+            buffer[i] = buffer[i + 1];
+        }
+
+        size--;
+
+        return removed;
+    }
+
+    /**
+     * Returns the value at the specified position in the list.
+     *
+     * @param index index of the value to return
+     * @return the value at the specified position in this list
+     * @throws IndexOutOfBoundsException if the index is out of range
+     */
+    @Override
+    public int get(int index)
+    {
+        return buffer[index];
+    }
+
+    /**
+     * Returns true if this list contains the specified value.
+     *
+     * @param value value whose presence in this list is to be searched for
+     * @return true if this list contains the specified value
+     */
+    @Override
+    public boolean contains(int value)
+    {
+
+        for (int i = 0; i < size; i++)
+        {
+            if(buffer[i] == value){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Returns the index of the first occurrence of the specified value
+     * in this list, or -1 if this list does not contain the value.
+     *
+     * @param value value to search for
+     * @return the index of the first occurrence of the specified value in this list
+     * or -1 if this list does not contain the value
+     */
+    @Override
+    public int indexOf(int value)
+    {
+        for (int i = 0; i < size; i++)
+        {
+            if(buffer[i] == value){
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    /**
+     * Returns true if this list contains no values.
+     *
+     * @return true if this list contains no values
+     */
+    @Override
+    public boolean isEmpty()
+    {
+            if(size == 0)
+            {
+                return true;
+            }
+
+        return false;
+    }
+
+    /**
+     * Returns the number of values in this list.
+     *
+     * @return the number of values in this list
+     */
+    @Override
+    public int size()
+    {
+        return size;
+    }
+
+    /**
+     * Removes all the values from this list.
+     * The list will be empty after this call returns.
+     */
+    @Override
+    public void clear()
+    {
+        buffer = new int[10];
+        size = 0;
+    }
+
+    /**
+     * Returns an iterator over elements of type {@code T}.
+     *
+     * @return an Iterator.
+     */
+    @Override
+    public Iterator<Integer> iterator()
+    {
+        return null;
+    }
+
+    /**
+     * Returns the contents of the ArrayIntList as a string
+     *
+     * @return
+     */
+    @Override
+    public String toString()
+    {
+        String content = "[ ";
+
+        for (int i = 0; i < size; i++)
+        {
+            content = content + buffer[i];
+            if(i != size - 1){
+                content = content + ", ";
+            }
+        }
+
+        content = content + " ]";
+
+        return content;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,64 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    ArrayIntList arrayIntList = new ArrayIntList();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        arrayIntList.addFront(5);
+        arrayIntList.addFront(4);
+        arrayIntList.addFront(3);
+        arrayIntList.addFront(2);
+        arrayIntList.addFront(1);
+        arrayIntList.addFront(0);
+
+        arrayIntList.addBack(6);
+        arrayIntList.addBack(7);
+        arrayIntList.addBack(8);
+        arrayIntList.addBack(9);
+
+        System.out.println("Printing array");
+        System.out.println(arrayIntList);
+
+        System.out.println("\nremove: Removing number from third index (should be removing number 3)");
+        arrayIntList.remove(3);
+        System.out.println(arrayIntList);
+
+        System.out.println("\nremoveFront: Removing number from the front (should be removing number 0)");
+        arrayIntList.removeFront();
+        System.out.println(arrayIntList);
+
+        System.out.println("\nremoveBack: Removing number from the end (should be removing number 9)");
+        arrayIntList.removeBack();
+        System.out.println(arrayIntList);
+
+        System.out.println("\nadd: adding 3 back to the array");
+        arrayIntList.add(2, 3);
+        System.out.println(arrayIntList);
+
+        System.out.println("\nget: getting value from index 0 (should be 1)");
+        System.out.println(arrayIntList.get(0));
+
+        System.out.println("\ncontains: checking if array contains 1 (should be true)");
+        System.out.println(arrayIntList.contains(1));
+
+        System.out.println("\ncontains: checking if array contains 10 (should be false)");
+        System.out.println(arrayIntList.contains(10));
+
+        System.out.println("\nindexOf: looking for first instance of the value 3 and getting its index (should be 2)");
+        System.out.println(arrayIntList.indexOf(3));
+
+        System.out.println("\nisEmpty: checking if array is empty (it should return false)");
+        System.out.println(arrayIntList.isEmpty());
+
+        ArrayIntList arrayIntListEmpty = new ArrayIntList();
+        System.out.println("\nisEmpty: checking if array is empty (it should return true)");
+        System.out.println(arrayIntListEmpty.isEmpty());
+
+        System.out.println("\nsize: returns size (should be 8)");
+        System.out.println(arrayIntList.size());
+
+        System.out.println("\nclear: array should be empty once this is called");
+        System.out.println("before: " + arrayIntList);
+        arrayIntList.clear();
+        System.out.println(" after: " + arrayIntList);
+
     }
 }
