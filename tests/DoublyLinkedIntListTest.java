6c77ce0bf9557d31a31e939c8402cf15d047facf
William Castillo <william.castillo.jr@outlook.com>
2024-01-24 15:44:14 -0800

    Revised and fixed addBack, add, and get methods

--- tests/DoublyLinkedIntListTest.java
+++ tests/DoublyLinkedIntListTest.java
@@ -4,10 +4,24 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class DoublyLinkedIntListTest
 {
+    public DoublyLinkedIntList createList()
+    {
+        DoublyLinkedIntList list = new DoublyLinkedIntList();
+        list.addFront(5);
+        list.add(1, 10);
+        list.add(2, 15);
+        list.add(3, 20);
+        list.addBack(25);
+
+        return list;
+    }
 
     @Test
     void addFront()
     {
+        DoublyLinkedIntList list = createList();
+        list.addFront(50);
+        assertEquals(50, list.get(0));
     }
 
     @Test
