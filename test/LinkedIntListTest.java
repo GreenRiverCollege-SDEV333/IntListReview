643defa54bc0ee077266f6d04051064082dc8899
jackle500 <78476113+jackle500@users.noreply.github.com>
2024-01-25 20:35:25 -0800

    finished remove, contains, and indexOf Method and generate test cases.

--- test/LinkedIntListTest.java
+++ test/LinkedIntListTest.java
@@ -42,14 +42,12 @@ class LinkedIntListTest {
         //10 20
         list.add(0,0);
         //0 10 20
+        list.add(1,30);
+        //0 30 10 20
         assertEquals(0, list.get(0));
-        assertEquals(10, list.get(1));
-        assertEquals(20, list.get(2));
-
-        list.add(2,100);
-        // 0 10 20 100
-        assertEquals(100, list.get(2));
-
+        assertEquals(30, list.get(1));
+        assertEquals(10, list.get(2));
+        assertEquals(20, list.get(3));
 
     }
 
@@ -78,6 +76,9 @@ class LinkedIntListTest {
         list.addFront(30);
         // 30 20 10
         list.removeBack();
+        //30 20
+        assertEquals(30, list.get(0));
+        assertEquals(20, list.get(1));
         assertEquals(2,list.size());
 
 
@@ -85,6 +86,15 @@ class LinkedIntListTest {
 
     @Test
     void remove() {
+        LinkedIntList list = new LinkedIntList();
+        list.addFront(10);
+        list.addFront(20);
+        list.addFront(30);
+        // 30 20 10
+        list.remove(1);
+        assertEquals(2, list.size());
+
+
     }
 
     @Test
@@ -101,10 +111,25 @@ class LinkedIntListTest {
 
     @Test
     void contains() {
+        LinkedIntList list = new LinkedIntList();
+        list.addBack(20);
+        list.addFront(5);
+        //5 20
+        assertTrue(list.contains(5));
+        assertEquals(2, list.size());
+        assertTrue(list.contains(20));
+        assertFalse(list.contains(15));
+
     }
 
     @Test
     void indexOf() {
+        LinkedIntList list = new LinkedIntList();
+        list.addBack(20);
+        list.addFront(5);
+        assertEquals(1, list.indexOf(20));
+        assertEquals(0, list.indexOf(5));
+
     }
 
     @Test
