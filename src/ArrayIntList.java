36925b625a27b640aefa2ed655ec8b089da02411
dhysdrn <dhysdrn@gmail.com>
2024-03-19 05:25:28 -0700

    Made minor changes to ArrayIntList

--- src/ArrayIntList.java
+++ src/ArrayIntList.java
@@ -28,7 +28,7 @@ public class ArrayIntList implements IntList {
         }
 
         for (int i = size; i >= 1; i--){
-            buffer[i + 1] = buffer[i];
+            buffer[i] = buffer[i - 1];
         }
 
         // put the value at the front of the array at position 0
